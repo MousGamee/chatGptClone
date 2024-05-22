fedbb99295cd8e864caa95bc23b976a65bcece4f
mousgame <moussa.sagna@hotmail.fr>
2024-05-23 00:07:53 +0200

    login

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,12 @@
 import AnimatedIntro from "@/components/AnimatedIntro";
+import BottomLoginSheet from "@/components/BottomLoginSheet";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <AnimatedIntro />
+    <View style={{ flex: 1 }}>
+      <AnimatedIntro />
+      <BottomLoginSheet />
+    </View>
   );
 }
